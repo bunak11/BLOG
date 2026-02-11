86864a2a2462c86aed0a6aabe7b2225f2551809a
Pavel Kovalev <bunak11@ya.ru>
2026-02-11 19:51:52 +0300

    Реализовать поиск статей

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,42 +1,65 @@
-import { useEffect, useState } from "react";
-import { Pagination, PostCard } from "./components";
+import { useMemo, useEffect, useState } from "react";
+import { Search, Pagination, PostCard } from "./components";
 import { useServerRequest } from "../../hooks";
 import { PAGINATION_LIMIT } from "../../constants";
-import { getLastPageFromLinks } from "./utils";
+import { debounce, getLastPageFromLinks } from "./utils";
 import styled from "styled-components";
 
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
 	const [page, setPage] = useState(1);
 	const [lastPage, setLastPage] = useState(1);
+	const [searchPhrase, setSearchPhrase] = useState("");
+	const [shouldSearch, setShouldSearch] = useState(false);
 	const requestServer = useServerRequest();
 
 	useEffect(() => {
-		requestServer("fetchPosts", page, PAGINATION_LIMIT).then(
+		requestServer("fetchPosts", searchPhrase, page, PAGINATION_LIMIT).then(
 			({ res: { posts, links } }) => {
 				setPosts(posts);
-
 				setLastPage(getLastPageFromLinks(links));
 			},
 		);
-	}, [requestServer, page]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [requestServer, page, shouldSearch]);
+
+	const startDelayedSearch = useMemo(
+		() => debounce(setShouldSearch, 2000),
+		[],
+	);
+
+	const onSearch = ({ target }) => {
+		setSearchPhrase(target.value);
+		startDelayedSearch(!shouldSearch);
+	};
 
 	return (
 		<div className={className}>
-			<div className="post-list">
-				{posts.map(
-					({ id, title, imageUrl, publishedAt, commentsCount }) => (
-						<PostCard
-							key={id}
-							id={id}
-							title={title}
-							imageUrl={imageUrl}
-							publishedAt={publishedAt}
-							commentsCount={commentsCount}
-						/>
-					),
-				)}
-			</div>
+			<Search searchPhrase={searchPhrase} onChange={onSearch} />
+			{posts.length ? (
+				<div className="post-list">
+					{posts.map(
+						({
+							id,
+							title,
+							imageUrl,
+							publishedAt,
+							commentsCount,
+						}) => (
+							<PostCard
+								key={id}
+								id={id}
+								title={title}
+								imageUrl={imageUrl}
+								publishedAt={publishedAt}
+								commentsCount={commentsCount}
+							/>
+						),
+					)}
+				</div>
+			) : (
+				<div className="no-post-found">Статьи не найдены</div>
+			)}
 			{lastPage > 1 && (
 				<Pagination page={page} lastPage={lastPage} setPage={setPage} />
 			)}
@@ -49,5 +72,11 @@ export const Main = styled(MainContainer)`
 		display: flex;
 		flex-wrap: wrap;
 		padding: 20px;
+	}
+
+	& .no-post-found {
+		text-align: center;
+		font-size: 18px;
+		margin-top: 40px;
 	}
 `;
--- src/pages/main/utils/get-last-page-from-links.js
+++ src/pages/main/utils/get-last-page-from-links.js
@@ -1,4 +1,16 @@
 export const getLastPageFromLinks = (links) => {
-	const result = links.match(/_page=(\d{1,4})&_limit=\d{1,3}>; rel="last"/);
-	return Number(result[1]);
+    if (!links || typeof links !== 'string') {
+        return 1;
+    }
+
+    // Разделяем по запятой и ищем часть с rel="last"
+    const lastLink = links.split(',').find(link => link.includes('rel="last"'));
+
+    if (!lastLink) {
+        return 1;
+    }
+
+    // Извлекаем номер страницы
+    const match = lastLink.match(/_page=(\d{1,4})/);
+    return match ? Number(match[1]) : 1;
 };
